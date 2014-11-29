419df8fa1195ffdaecc7eef852d2a46e1b6007ea
aviran3369 <aviran3369@gmail.com>
2014-11-29 23:28:15 +0200

    WorkoMVC added

---
+++ Worko/WorkoMVC/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WorkoMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Empty()
+        {
+            return View();
+        }
+
+        public ActionResult AngularTest()
+        {
+            return View("AngularTest");
+        }
+    }
+}
---
+++ Worko/WorkoMVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WorkoMVC.MvcApplication" Language="C#" %>
